d9a70ba2aeec90b3d6b99a24a7cddcda9b8747b9
Brenon <eibrenon88@gmail.com>
2023-04-06 21:46:20 -0300

    Adicionando mensagem

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando ao banco de dados")
 console.log("O paulo fez essa alteraçao")
+console.log("Enviando para o github")
