d87cf366eea5f7cb10b88adf61af6d01e4baf42a
Brenon <eibrenon88@gmail.com>
2023-04-06 22:47:55 -0300

    Alteração na linha para teste

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando ao banco de dados")
 console.log("O paulo fez essa alteraçao")
-console.log("modificação adicional")
+console.log("modificação adicional!")
 
